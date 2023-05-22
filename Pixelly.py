18ac7a4c61cf1db5691e9799804aa9d4643e8bbe
Harsh Nanda <harshnanda47@gmail.com>
2023-05-22 22:46:46 +0530

    added contact us page and integrated the website

--- Pixelly.py
+++ Pixelly.py
@@ -36,5 +36,9 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
